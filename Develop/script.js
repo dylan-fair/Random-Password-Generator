84933a458b6618dec865a0c59f76f6e8594b595f
dylan fair <dylanfair11@gmail.com>
2021-09-06 16:13:45 -0500

    added messages for length and which charaters to include

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,36 @@
 // Assignment code here
-
-
+var groups = ["abcdefghijklmnopqrstuvwxy","ABCDEFGHIJKLMNOPQRSTUVWXYZ","1234567890","!#$%&'()*,-./:;<=>?@[/]^_{|}~`"]
+var check = {
+  lower: false,
+  upper: false,
+  num: false,
+  special: false
+}
+totalElements = "";
+var generatePassword = function() {
+  passwordLength = 0;
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("how long would you like your password? between 8 and 128");
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please enter a value between 8 and 128!");
+    }
+  }
+  while (check.lower === false && check.upper === false && check.num === false && check.special === false) {
+    check.lower = window.confirm("Would you like Lowerase letters included?");
+    check.upper = window.confirm("Would you like Uppercase letters included?");
+    check.num = window.confirm("Would you like numbers included?")
+    check.special = window.confirm("Would you like special characters included?");
+  }
+  var i = 0;
+  for (key in check) {
+    console.log(groups[i]);
+    if (check[key]) {
+      totalElements = totalElements + groups[i];
+    }
+    i = i +1;
+  }
+}
+console.log(totalElements);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
